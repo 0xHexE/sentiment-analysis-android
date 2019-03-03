3306b04e8bae575a7d894df89225de10abfdab33
Omkar Yadav <httpsomkar@gmail.com>
2019-03-03 08:43:06 +0530

    Fixed

--- app/src/main/java/com/omkar/sentiment/MainActivity.java
+++ app/src/main/java/com/omkar/sentiment/MainActivity.java
@@ -14,15 +14,16 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    this.summaryGenerator("John is great");
-    this.summaryGenerator("John is great also awesome");
+    this.summaryGenerator("John is great", "test");
+    this.summaryGenerator("John is great also awesome", "Test");
   }
 
-  String summaryGenerator(String myInputString) {
+  String summaryGenerator(String myInputString, String title) {
     TextAPIClient client = new TextAPIClient("785ffb7d", "feb9d044e201df8c926602346d98de68");
 
     SummarizeParams.Builder builder = SummarizeParams.newBuilder();
     builder.setText(myInputString);
+    builder.setTitle(title);
 
     builder.setNumberOfSentences(2);
 
